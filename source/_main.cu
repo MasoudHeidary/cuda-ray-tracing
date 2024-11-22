e8940ea2e79aca38c1509dbe5f88baf5d98ea0a3
Masoud Heidary <Masoud.hei.dev@gmail.com>
2024-11-22 12:50:21 -0600

    CUDA & CPU code

--- source/_main.cu
+++ source/_main.cu
@@ -41,15 +41,12 @@ __global__ void render_image(
         float u = float(image_width - 1 - i) / float(image_width - 1);
         float v = float(image_height - 1 - j) / float(image_height - 1); // Flip the v coordinate
 
-        // Create the ray from the camera
-        // Ray ray(camera_position, direction_of_camera);  // Define your camera position and direction
         Ray ray = camera.get_ray(u, v);
 
         glm::vec3 pixel_color = background;
         Hit closest_hit_record;
         closest_hit_record.hit = false;
 
-        // Iterate through spheres for intersection (simple sphere intersection for now)
         Hit hit_record;
         for (int i = 0; i < num_spheres; i++)
         {
@@ -90,28 +87,11 @@ __global__ void render_image(
         {
             glm::vec3 point = ray.at(closest_hit_record.t);
             glm::vec3 lighting = compute_lighting(point, closest_hit_record.normal, lights, num_lights);
-            // if (i == 500 && j == 500) {
-            //     printf("num lights: %d\n", num_lights);
-            //     printf("lightning color: %f %f %f\n", lighting.r, lighting.g, lighting.b);
-            // }
             pixel_color = glm::clamp(closest_hit_record.color * lighting, 0.0f, 1.0f);
         }
-        // else {
-        //     pixel_color = background;  // If no intersection, use the background color
-        // }
 
-        // Set pixel color in the image (CUDA memory access)
         image[j * image_width + i] = pixel_color;
 
-        // if (i == 1 && j == 1)
-        // {
-        //     printf("Ray origin: (%f, %f, %f)\n", ray.origin.x, ray.origin.y, ray.origin.z);
-        //     printf("Ray direction: (%f, %f, %f)\n", ray.direction.x, ray.direction.y, ray.direction.z);
-        //     printf("Closest hit: %d, T: %f\n", closest_hit_record.hit, closest_hit_record.t);
-        //     printf("Closest hit color: %f %f %f\n", closest_hit_record.color.r, closest_hit_record.color.g, closest_hit_record.color.b);
-        //     printf("plane color: %f %f %f\n", planes[0].color.r, planes[0].color.g, planes[0].color.b);
-        //     printf("pixel color: %f %f %f\n", pixel_color.r, pixel_color.g, pixel_color.b);
-        // }
     }
 }
 
@@ -175,41 +155,34 @@ int _main(int argc, char *argv[])
     std::cout << std::endl;
 
     auto start_time = high_resolution_clock::now();
+    auto end_time = high_resolution_clock::now();
+    double total_render_time = duration<double>(end_time - start_time).count();
 
+    // ==================== CUDA ====================
     if (cuda_enable)
     {
+        start_time = high_resolution_clock::now();
+
         glm::vec3 *d_image;
         cudaMalloc(&d_image, image_width * image_height * sizeof(glm::vec3));
 
-        // Define spheres and lights
-        // std::vector<Sphere> spheres;
-        // std::vector<Light> lights;
-
-        // // Add some spheres and lights
-        // spheres.push_back(Sphere(glm::vec3(0.0f, 0.0f, -5.0f), 1.0f, glm::vec3(1.0f, 0.0f, 0.0f)));
-        // lights.push_back(Light{glm::vec3(5.0f, 5.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f)});
-
-        // Allocate memory for spheres and lights on the device
-        Sphere *d_spheres;
-        cudaMalloc(&d_spheres, spheres.size() * sizeof(Sphere));
-        Plane *d_planes;
-        cudaMalloc(&d_planes, planes.size() * sizeof(Plane));
-        Triangle *d_triangles;
-        cudaMalloc(&d_triangles, triangles.size() * sizeof(Triangle));
         Light *d_lights;
+        Sphere *d_spheres;
+        Plane *d_planes;
+        Triangle *d_triangles;
         cudaMalloc(&d_lights, lights.size() * sizeof(Light));
+        cudaMalloc(&d_spheres, spheres.size() * sizeof(Sphere));
+        cudaMalloc(&d_planes, planes.size() * sizeof(Plane));
+        cudaMalloc(&d_triangles, triangles.size() * sizeof(Triangle));
 
-        // Copy spheres and lights data to device
+        cudaMemcpy(d_lights, lights.data(), lights.size() * sizeof(Light), cudaMemcpyHostToDevice);
         cudaMemcpy(d_spheres, spheres.data(), spheres.size() * sizeof(Sphere), cudaMemcpyHostToDevice);
         cudaMemcpy(d_planes, planes.data(), planes.size() * sizeof(Plane), cudaMemcpyHostToDevice);
         cudaMemcpy(d_triangles, triangles.data(), triangles.size() * sizeof(Triangle), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_lights, lights.data(), lights.size() * sizeof(Light), cudaMemcpyHostToDevice);
 
-        // Define CUDA block and grid sizes
         dim3 threadsPerBlock(16, 16);
         dim3 numBlocks((image_width + 15) / 16, (image_height + 15) / 16);
 
-        // Launch the kernel
         render_image<<<numBlocks, threadsPerBlock>>>(
             camera,
             d_image,
@@ -225,10 +198,8 @@ int _main(int argc, char *argv[])
             lights.size(),
             background);
 
-        // Check for CUDA errors
         cudaDeviceSynchronize();
 
-        // Copy the result from device to host
         glm::vec3 *h_image = new glm::vec3[image_width * image_height];
         cudaMemcpy(h_image, d_image, image_width * image_height * sizeof(glm::vec3), cudaMemcpyDeviceToHost);
 
@@ -238,64 +209,45 @@ int _main(int argc, char *argv[])
         cudaFree(d_triangles);
         cudaFree(d_lights);
 
-        // End total render time
-        auto end_time = high_resolution_clock::now();
-        double total_render_time = duration<double>(end_time - start_time).count();
+        end_time = high_resolution_clock::now();
+        total_render_time = duration<double>(end_time - start_time).count();
 
-        // Output profiling results
-        std::cout << std::fixed << std::setprecision(6);
-        std::cout << "Total render time: " << total_render_time << " seconds" << std::endl;
-
-        // Save or display the image (e.g., using OpenCV or another library)
-        // Convert h_image (glm::vec3*) to cv::Mat
-        cv::Mat image(image_height, image_width, CV_8UC3);
 
         for (int j = 0; j < image_height; ++j)
         {
             for (int i = 0; i < image_width; ++i)
             {
                 glm::vec3 color = h_image[j * image_width + i];
-                image.at<cv::Vec3b>(j, i)[0] = static_cast<unsigned char>(255.0f * color.b); // Blue channel
-                image.at<cv::Vec3b>(j, i)[1] = static_cast<unsigned char>(255.0f * color.g); // Green channel
-                image.at<cv::Vec3b>(j, i)[2] = static_cast<unsigned char>(255.0f * color.r); // Red channel
-                // std::cout << image.at<cv::Vec3b>(j, i) << std::endl;
+                image.at<cv::Vec3b>(j, i)[0] = static_cast<unsigned char>(255.0f * color.b);
+                image.at<cv::Vec3b>(j, i)[1] = static_cast<unsigned char>(255.0f * color.g);
+                image.at<cv::Vec3b>(j, i)[2] = static_cast<unsigned char>(255.0f * color.r);
             }
         }
 
-        // Save the image to file
-        if (cv::imwrite(output_file_name, image))
-        {
-            std::cout << "Image saved successfully to " << output_file_name << std::endl;
-        }
-        else
-        {
-            std::cerr << "Error: Could not save the image!" << std::endl;
-        }
-
-        // Clean up
         delete[] h_image;
     }
+
+    // ==================== CPU ====================
     else
     {
+        start_time = high_resolution_clock::now();
+
         // chunk render
         auto render_chunk = [&](int start_row, int end_row, int thread_id)
         {
             // Render loop
             for (int j = start_row; j < end_row; ++j)
             {
-                // std::cout << "Rendering row " << "[" << j << "/" << image_height << "]" << std::endl;
                 for (int i = 0; i < image_width; ++i)
                 {
-                    // auto pixel_start_time = high_resolution_clock::now();  // Start timing pixel
 
+                    //flip coordinate
                     float u = float(image_width - 1 - i) / float(image_width - 1);
-                    float v = float(image_height - 1 - j) / (image_height - 1); // Flip the v coordinate
+                    float v = float(image_height - 1 - j) / (image_height - 1);
 
                     Ray ray = camera.get_ray(u, v);
                     glm::vec3 pixel_color = background;
-                    Hit closest_hit_record; // Store the closest hit information
-                    bool hit_anything = false;
-                    float closest_t = std::numeric_limits<float>::max(); // Set to a very large number initially
+                    Hit closest_hit_record;
 
                     // Iterate through each sphere to find the closest intersection
                     for (const Sphere &sphere : spheres)
@@ -303,11 +255,9 @@ int _main(int argc, char *argv[])
                         Hit hit_record;
                         if (sphere.intersect(ray, hit_record))
                         {
-                            if (hit_record.t < closest_t)
+                            if (hit_record.t < closest_hit_record.t)
                             {
-                                closest_t = hit_record.t;
                                 closest_hit_record = hit_record;
-                                hit_anything = true;
                             }
                         }
                     }
@@ -318,11 +268,9 @@ int _main(int argc, char *argv[])
                         Hit hit_record;
                         if (plane.intersect(ray, hit_record))
                         {
-                            if (hit_record.t < closest_t)
+                            if (hit_record.t < closest_hit_record.t)
                             {
-                                closest_t = hit_record.t;
                                 closest_hit_record = hit_record;
-                                hit_anything = true;
                             }
                         }
                     }
@@ -333,22 +281,18 @@ int _main(int argc, char *argv[])
                         Hit hit_record;
                         if (triangle.intersect(ray, hit_record))
                         {
-                            if (hit_record.t < closest_t)
+                            if (hit_record.t < closest_hit_record.t)
                             {
-                                closest_t = hit_record.t;
                                 closest_hit_record = hit_record;
-                                hit_anything = true;
                             }
                         }
                     }
 
                     // Compute lighting and color based on the closest hit
-                    if (hit_anything)
+                    if (closest_hit_record.hit)
                     {
                         glm::vec3 point = ray.at(closest_hit_record.t);
 
-                        // Measure lighting calculation time
-                        // auto lighting_start_time = high_resolution_clock::now();
                         glm::vec3 lighting;
                         if (shadow_enable)
                         {
@@ -366,9 +310,9 @@ int _main(int argc, char *argv[])
                         pixel_color = background; // If no intersection, use the background color
                     }
 
-                    image.at<cv::Vec3b>(j, i)[0] = static_cast<unsigned char>(255.0 * pixel_color.b); // Blue channel
-                    image.at<cv::Vec3b>(j, i)[1] = static_cast<unsigned char>(255.0 * pixel_color.g); // Green channel
-                    image.at<cv::Vec3b>(j, i)[2] = static_cast<unsigned char>(255.0 * pixel_color.r); // Red channel
+                    image.at<cv::Vec3b>(j, i)[0] = static_cast<unsigned char>(255.0 * pixel_color.b); 
+                    image.at<cv::Vec3b>(j, i)[1] = static_cast<unsigned char>(255.0 * pixel_color.g); 
+                    image.at<cv::Vec3b>(j, i)[2] = static_cast<unsigned char>(255.0 * pixel_color.r); 
                 }
             }
 
@@ -377,7 +321,7 @@ int _main(int argc, char *argv[])
 
         // Launch threads
         std::vector<std::thread> threads;
-        int rows_per_thread = image_height / num_threads; // Divide the rows evenly among threads
+        int rows_per_thread = image_height / num_threads;
         for (unsigned int t = 0; t < num_threads; ++t)
         {
             int start_row = t * rows_per_thread;
@@ -385,7 +329,7 @@ int _main(int argc, char *argv[])
             threads.push_back(std::thread(render_chunk, start_row, end_row, t));
         }
 
-        // Join threads (wait for them to finish)
+        // Join threads
         for (std::thread &thread : threads)
         {
             if (thread.joinable())
@@ -393,47 +337,24 @@ int _main(int argc, char *argv[])
                 thread.join();
             }
         }
-        for (unsigned int t = 0; t < num_threads; ++t)
-        {
-            // std::cout << "Thread " << t << " execution time: " << thread_execution_times[t] << " seconds" << std::endl;
-        }
 
-        // End total render time
-        auto end_time = high_resolution_clock::now();
-        double total_render_time = duration<double>(end_time - start_time).count();
-
-        // Output profiling results
-        std::cout << std::fixed << std::setprecision(6);
-        std::cout << "Total render time: " << total_render_time << " seconds" << std::endl;
-        // std::cout << "Average time per pixel: " << total_pixel_time / (image_width * image_height) << " seconds" << std::endl;
-        // std::cout << "Total lighting calculation time: " << total_lighting_time << " seconds" << std::endl;
-        // std::cout << "Average lighting calculation time per pixel: " << total_lighting_time / (image_width * image_height) << " seconds" << std::endl;
-
-        // open the picture after saving
-        // image.save(output_file_name);
-        if (cv::imwrite(output_file_name, image))
-        {
-            std::cout << "Image saved successfully to " << output_file_name << std::endl;
-        }
-        else
-        {
-            std::cout << "Error: Could not save the image!" << std::endl;
-        }
+        end_time = high_resolution_clock::now();
+        total_render_time = duration<double>(end_time - start_time).count();
     }
 
-    // // End total render time
-    // auto end_time = high_resolution_clock::now();
-    // double total_render_time = duration<double>(end_time - start_time).count();
 
-    // // Output profiling results
-    // std::cout << std::fixed << std::setprecision(6);
-    // std::cout << "Total render time: " << total_render_time << " seconds" << std::endl;
 
-    // if (cv::imwrite(output_file_name, image)) {
-    //     std::cout << "Image saved successfully to " << output_file_name << std::endl;
-    // } else {
-    //     std::cout << "Error: Could not save the image!" << std::endl;
-    // }
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "Total render time: " << total_render_time << " seconds" << std::endl;
+
+    if (cv::imwrite(output_file_name, image))
+    {
+        std::cout << "Image saved successfully to " << output_file_name << std::endl;
+    }
+    else
+    {
+        std::cerr << "Error: Could not save the image!" << std::endl;
+    }
 
     return 0;
 }
